f604ff7075c5c46976135243497c9cf62ad19b3e
khavayimagomere <khavayimag@gmail.com>
2017-09-20 12:08:33 +0300

    music player

--- app.js
+++ app.js
@@ -2,6 +2,20 @@ let player = new AudioPlayer();
 
 function renderItems(data) {
     // render items to the browser
+    const ul = document.querySelector('ul.playlist');
+
+    ul.innerHTML = '';
+
+    data.forEach(function(item){
+    	const li = document.createElement('li');
+    	li.textContent = item.trackName;
+
+    	li.onclick = (e)=>{
+    		player.play(item);
+    	}
+
+    	ul.appendChild(li);
+    })
 }
 
 
@@ -9,10 +23,31 @@ const searchForm = document.querySelector('form');
 
 searchForm.onsubmit = (e) => {
 
+	console.log('my form was submitted')
+
     // prevent normal form submission
     e.preventDefault();
 
     // fetch items here
 
-    let endpoint = 'https://itunes.apple.com/search?term=bob+dylan';
+   
+    let artistName = document.querySelector('#artist').value;
+
+     let endpoint = 'https://itunes.apple.com/search?term=' + artistName;
+
+
+    const xhr = new XMLHttpRequest();
+
+    xhr.addEventListener('load', (e)=>{
+    	let response = JSON.parse(xhr.responseText);
+    	
+    	renderItems(response.results);
+
+    	console.log("done")
+   
+   });
+
+    xhr.open('GET', endpoint, true);
+
+    xhr.send();
 }
